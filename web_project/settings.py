4a4545fac756d7511bfc5e840d40b27ac8de970d
mcgannstel <stellajanemcgann@gmail.com>
2026-03-10 21:43:38 +1300

    Prepare project for Render deployment (again)

--- web_project/settings.py
+++ web_project/settings.py
@@ -100,3 +100,15 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/login/"
+
+import os
+from django.contrib.auth import get_user_model
+
+if os.environ.get("CREATE_SUPERUSER", "False") == "True":
+    User = get_user_model()
+    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+    if username and password and not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
